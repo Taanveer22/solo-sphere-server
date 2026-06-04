1d6ba7c8ec94ef5488f8bb7412b09ef7b530125d
Taanveer22 <taanveer469@gmail.com>
2026-06-05 05:25:42 +0600

    alhamdulliah read operation done for one job

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -38,6 +38,12 @@ async function run() {
     app.get('/jobs', async (req, res) => {
       const cursor = jobsCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/jobs/:id', async (req, res) => {
+      const query = { _id: new ObjectId(req.params.id) };
+      const result = await jobsCollection.findOne(query);
       res.send(result);
     });
 
