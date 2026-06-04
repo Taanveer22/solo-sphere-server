d8b11558c12a6974744ebb593808d462d455c292
Taanveer22 <taanveer469@gmail.com>
2026-06-05 04:41:54 +0600

    alhamdulliah all jobs get api created done

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.89rnkti.mongodb.net/?appName=Cluster0`;
-console.log(uri);
+// console.log(uri);
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -22,16 +28,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server
-    await client.connect();
-    console.log('You successfully connected to MongoDB');
-    // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 });
-    console.log('Pinged your deployment');
+    // await client.connect();
+    console.log('connect client to server');
+    // =======================================================================
+    const db = client.db('soloSphereDB');
+    const jobsCollection = db.collection('jobsColl');
+
+    // =======================================================================
+    app.get('/jobs', async (req, res) => {
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // =======================================================================
+    // await client.db('admin').command({ ping: 1 });
+    console.log('Send a ping to confirm a successful connection');
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-    console.log('Finished task from MongoDB');
+    // await client.close();
+    console.log('Ensures that the client will close when you finish/error');
   }
 }
 run().catch(console.dir);
